22c9cc8abee530a34bff3cdfbbb91d04760bccaa
aoponcedeleon <aoponcedeleon@up.edu.ph>
2019-06-29 14:00:26 +0800

    Step 1

---
+++ src/components/users/UserItem.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const UserItem = props => {
+  const { login, avatar_url, html_url } = props.user;
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt=''
+        className='round-img'
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className='btn btn-dark btn-sm my-1'>
+          More
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
