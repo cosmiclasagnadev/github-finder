e22b361d1a35f523ab885d2f5fe8703cb75b6122
aoponcedeleon <aoponcedeleon@up.edu.ph>
2019-07-03 16:11:20 +0800

    Prepared for deployment

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from "./components/users/User";
 import Alert from "./components/layout/Alert";
 import Home from "./components/pages/Home";
 import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 import GithubState from "./context/github/GithubState";
 import AlertState from "./context/alert/AlertState";
@@ -24,6 +25,7 @@ const App = () => {
                 <Route exact path='/' component={Home} />
                 <Route exact path='/about' component={About} />
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
             <h1> </h1>
